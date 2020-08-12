f61e9e9afb6c5ccba84821f86ac8c9fc4b3bb83c
tomohiro-in <1341hirunetyu@gmail.com>
2020-08-12 21:45:58 +0900

    初めてのコミット

---
+++ kaihouhikaku.js
@@ -0,0 +1,9 @@
+"use strict";
+window.addEventListener("load", () => {
+  var mui = new kaihouhikaku(
+    document.querySelector("#start"),
+    document.querySelector("#clear"),
+    document.querySelector("stop"),
+    document.querySelector("#graph1"),
+    document.querySelector("#graph1b")
+  );
